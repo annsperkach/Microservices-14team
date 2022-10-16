0ad8f3461406b39f3bd14cc94cc963f8b156daec
GreenSky <71709949+TrachukYulia@users.noreply.github.com>
2022-10-17 04:54:04 +0500

    Add files via upload

---
+++ 1 lab/UserService/Models/UserServiceContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Models
+{
+    public class UserServiceContext : DbContext
+    {
+        public UserServiceContext(DbContextOptions<UserServiceContext> options)
+            : base(options)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
